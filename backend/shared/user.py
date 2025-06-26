198254f56088c18949a22129cabccc9147314189
jhubs-stack <jhubsche@gmail.com>
2025-06-26 14:33:56 -0600

    Clean slate for YC MVP

--- backend/shared/user.py
+++
@@ -1,8 +0,0 @@
-# shared/user.py
-
-def get_current_user():
-    return {
-        "id": "user_123",
-        "name": "Demo User",
-        "created_at": "2025-06-10"
-    }
--- backend/utils/helpers.py
+++
@@ -1,4 +0,0 @@
-# utils/helpers.py
-
-def format_timestamp(ts):
-    return ts.strftime("%Y-%m-%d %H:%M")
